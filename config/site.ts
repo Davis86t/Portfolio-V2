4d775e27ba6e8efed6d165c8c9d1f8ffe7034c38
davis86t <4833583@gmail.com>
2023-04-11 11:14:29 -0500

    initial commit

---
+++ config/site.ts
@@ -0,0 +1,31 @@
+import { NavItem } from "@/types/nav"
+
+interface SiteConfig {
+  name: string
+  description: string
+  mainNav: NavItem[]
+  links: {
+    twitter: string
+    github: string
+    docs: string
+    linkedin: string
+  }
+}
+
+export const siteConfig: SiteConfig = {
+  name: "Tanner Davis",
+  description:
+    "Tanner Davis' personal website. I'm a software engineer and I love to build things.",
+  mainNav: [
+    {
+      title: "Home",
+      href: "/",
+    },
+  ],
+  links: {
+    twitter: "https://twitter.com/shadcn",
+    github: "https://github.com/davis86t",
+    docs: "https://ui.shadcn.com",
+    linkedin: "https://www.linkedin.com/in/davis86t/",
+  },
+}
