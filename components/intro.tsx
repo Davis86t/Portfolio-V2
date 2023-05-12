9e5863750ede911d6239426746d7e35a55765472
davis86t <4833583@gmail.com>
2023-05-12 12:03:46 -0500

    forgot to remove borders after styling

--- components/intro.tsx
+++ components/intro.tsx
@@ -11,14 +11,14 @@ export default function Intro() {
     <>
       <section
         id="home"
-        className="z-0 mt-[-65px] flex min-h-screen flex-col justify-center border pb-[15px] sm:mt-0 sm:min-h-[calc(100vh-65px)] sm:pb-0"
+        className="z-0 mt-[-65px] flex min-h-screen flex-col justify-center pb-[15px] sm:mt-0 sm:min-h-[calc(100vh-65px)] sm:pb-0"
         style={
           {
             // height: `calc(100vh - 65px - env(safe-area-inset-top) - env(safe-area-inset-bottom))`,
           }
         }
       >
-        <div className=" flex h-full flex-col items-center border sm:mb-[100px] sm:pt-[8vh]">
+        <div className=" flex h-full flex-col items-center sm:mb-[100px] sm:pt-[8vh]">
           <section className="container flex h-full flex-col justify-start pt-4 sm:items-start sm:justify-center sm:pt-0">
             <div className="flex flex-col items-center gap-6 sm:flex-row sm:pb-6 md:pb-0">
               <AvatarAnimation />
