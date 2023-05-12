6c2cba2d8d43da099df57de7225a40916dc063c7
davis86t <4833583@gmail.com>
2023-05-12 11:12:50 -0500

    still testing mobile browser navbar issues

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,9 +2,9 @@ import { Head, Html, Main, NextScript } from "next/document"
 
 export default function Document() {
   return (
-    <Html className="h-full" lang="en">
+    <Html className="h-[100%]" lang="en">
       <Head />
-      <body className="h-full w-[100%] overflow-x-hidden bg-white font-sans text-slate-900 antialiased dark:bg-slate-900 dark:text-slate-50">
+      <body className="h-[100%] w-[100%] overflow-x-hidden bg-white font-sans text-slate-900 antialiased dark:bg-slate-900 dark:text-slate-50">
         <Main />
         <NextScript />
       </body>
