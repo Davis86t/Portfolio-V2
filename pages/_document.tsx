a182f19b4f7815a0c64e888809a220d62ce3a403
davis86t <4833583@gmail.com>
2023-05-12 19:13:25 -0500

    overflow edit

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html className="h-[100%]" lang="en">
       <Head />
-      <body className="h-[100%] w-[100%] overflow-x-clip bg-white font-sans text-slate-900 antialiased dark:bg-slate-900 dark:text-slate-50">
+      <body className="h-[100%] w-[100%] overflow-x-hidden bg-white font-sans text-slate-900 antialiased dark:bg-slate-900 dark:text-slate-50">
         <Main />
         <NextScript />
       </body>
