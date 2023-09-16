bfb48fd0acb141b4c615fc5df6f7f3451b6fec04
davis86t <4833583@gmail.com>
2023-09-15 23:37:49 -0500

    apostrophe

--- components/about.tsx
+++ components/about.tsx
@@ -21,16 +21,16 @@ export default function AboutMe() {
           <AboutMeAnimation />
           <div className="flex max-h-[55vh] flex-col overflow-scroll rounded-lg bg-slate-100 p-4 dark:bg-slate-800 sm:m-0 sm:justify-center sm:overflow-auto sm:bg-white sm:p-0 sm:dark:bg-slate-900">
             <p className="text-md max-w-[700px] pb-3 text-slate-700 dark:text-slate-400 md:text-xl">
-              Hi, I'm Tanner Davis, a Full Stack Developer with a foundation in electronics. My expertise includes creating web apps with Node.js, Express, React.js, Redux, and Next.js, and managing SQL databases like Postgres. I love taking on tech challenges.
+              Hi, I&apos;m Tanner Davis, a Full Stack Developer with a foundation in electronics. My expertise includes creating web apps with Node.js, Express, React.js, Redux, and Next.js, and managing SQL databases like Postgres. I love taking on tech challenges.
             </p>
             <p className="text-md max-w-[700px] pb-3 text-slate-700 dark:text-slate-400 md:text-xl">
-              Before transitioning to software, I was an electronics technician, ran a home construction business, and worked as a realtor. This diverse background strengthened my problem-solving and management skills, which I'm excited to apply to software development.
+              Before transitioning to software, I was an electronics technician, ran a home construction business, and worked as a realtor. This diverse background strengthened my problem-solving and management skills, which I&apos;m excited to apply to software development.
             </p>
             <p className="text-md max-w-[700px] pb-3 text-slate-700 dark:text-slate-400 md:text-xl">
-              Off the clock, I'm a car enthusiast and PC gamer. These hobbies not only satisfy my curiosity but also help me unwind. My biggest motivation and inspiration is my family.
+              Off the clock, I&apos;m a car enthusiast and PC gamer. These hobbies not only satisfy my curiosity but also help me unwind. My biggest motivation and inspiration is my family.
             </p>
             <p className="text-md max-w-[700px] text-slate-700 dark:text-slate-400 md:text-xl">
-            Feel free to reach out and connect if you're interested in discussing potential opportunities, the world of software engineering, or just to chat. Thanks for stopping by!
+            Feel free to reach out and connect if you&apos;re interested in discussing potential opportunities, the world of software engineering, or just to chat. Thanks for stopping by!
             </p>
           </div>
           <Link
