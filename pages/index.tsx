31473e35e668f17d8c7916e51ae781872f62e1d5
davis86t <4833583@gmail.com>
2023-11-14 16:43:30 -0600

    Add favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -39,7 +39,7 @@ export default function IndexPage() {
         <title>Tanner Davis</title>
         <meta name="description" content="My Personal Website." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/TD4.png" />
         <meta property="og:type" content="website" />
         <meta property="og:title" content="Tanner Davis | Portfolio" />
         <meta property="og:description" content="Welcome to my personal portfolio where I showcase my projects and professional journey." />
