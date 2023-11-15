d00d55a6482eb91653d23947efd7517105bba8ce
davis86t <4833583@gmail.com>
2023-11-15 17:18:49 -0600

    Changed main-nav links to temporarily change color on hover. Refactored main-nav code to reduce redundancy

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -1,17 +1,14 @@
-import React, { useEffect, useRef } from "react"
-import { Link, animateScroll as scroll } from "react-scroll"
+import React, { useEffect, useState } from "react"
+import { Link } from "react-scroll"
 
 import { NavItem } from "@/types/nav"
 import { siteConfig } from "@/config/site"
-import { cn } from "@/lib/utils"
 import { Icons } from "@/components/icons"
 import { Button } from "@/components/ui/button"
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 
@@ -19,39 +16,33 @@ interface MainNavProps {
   items?: NavItem[]
 }
 
+const navItems = [
+  { to: "home", label: "Home" },
+  { to: "portfolio", label: "Portfolio" },
+  { to: "aboutMe", label: "About Me" },
+  { to: "contact", label: "Contact" },
+]
+
 export function MainNav({ items }: MainNavProps) {
-  const resumeLinkRef = useRef(null)
+  const [isDarkMode, setIsDarkMode] = useState(false)
 
   useEffect(() => {
-    // Function to remove the hover state
-    const removeHoverState = () => {
-      // Remove the attribute after a delay to simulate hover effect
-      setTimeout(() => {
-        if (resumeLinkRef.current) {
-          resumeLinkRef.current.removeAttribute("data-hover")
-        }
-      }, 500) // Delay in milliseconds
+    const matchDarkMediaQuery = window.matchMedia(
+      "(prefers-color-scheme: dark)"
+    )
+
+    const handleChange = (event) => {
+      setIsDarkMode(event.matches)
     }
 
-    // Function to add the hover state
-    const addHoverState = () => {
-      if (resumeLinkRef.current) {
-        resumeLinkRef.current.setAttribute("data-hover", "true")
-      }
-    }
+    // Listen for changes using addEventListener
+    matchDarkMediaQuery.addEventListener("change", handleChange)
 
-    // Get the current element from the ref
-    const resumeLink = resumeLinkRef.current
+    // Set the initial state based on the current color scheme
+    setIsDarkMode(matchDarkMediaQuery.matches)
 
-    // Add event listeners
-    resumeLink.addEventListener("touchend", removeHoverState)
-    resumeLink.addEventListener("touchstart", addHoverState)
-
-    // Remove event listeners on cleanup
-    return () => {
-      resumeLink.removeEventListener("touchend", removeHoverState)
-      resumeLink.removeEventListener("touchstart", addHoverState)
-    }
+    // Clean up with removeEventListener
+    return () => matchDarkMediaQuery.removeEventListener("change", handleChange)
   }, [])
 
   function onClick() {
@@ -62,64 +53,47 @@ export function MainNav({ items }: MainNavProps) {
     <div className="flex gap-6 md:gap-10">
       {items?.length ? (
         <nav className="hidden gap-6 md:flex">
-          <Link
-            activeClass="border-b-2 border-slate-600"
-            to="home"
-            spy={true}
-            smooth="easeOutQuad"
-            offset={-65}
-            duration={750}
-            className={cn(
-              "flex cursor-pointer items-center text-lg font-semibold text-slate-600 hover:text-slate-900 dark:text-slate-100 dark:hover:text-slate-500 sm:text-sm"
-            )}
-          >
-            Home
-          </Link>
-          <Link
-            activeClass="border-b-2 border-slate-600"
-            to="portfolio"
-            spy={true}
-            smooth="easeOutQuad"
-            offset={-65}
-            duration={750}
-            className={cn(
-              "flex cursor-pointer items-center text-lg font-semibold text-slate-600 hover:text-slate-900 dark:text-slate-100 dark:hover:text-slate-500 sm:text-sm"
-            )}
-          >
-            Portfolio
-          </Link>
-          <Link
-            activeClass="border-b-2 border-slate-600"
-            to="aboutMe"
-            spy={true}
-            smooth="easeOutQuad"
-            offset={-65}
-            duration={750}
-            className={cn(
-              "flex cursor-pointer items-center text-lg font-semibold text-slate-600 hover:text-slate-900 dark:text-slate-100 dark:hover:text-slate-500 sm:text-sm"
-            )}
-          >
-            About Me
-          </Link>
-          <Link
-            activeClass="border-b-2 border-slate-600"
-            to="contact"
-            spy={true}
-            smooth="easeOutQuad"
-            offset={-65}
-            duration={750}
-            className={cn(
-              "flex cursor-pointer items-center text-lg font-semibold text-slate-600 hover:text-slate-900 dark:text-slate-100 dark:hover:text-slate-500 sm:text-sm"
-            )}
-          >
-            Contact
-          </Link>
+          {navItems.map((item) => (
+            <Link
+              key={item.to}
+              activeClass="border-b-2 border-slate-600"
+              to={item.to}
+              spy={true}
+              smooth="easeOutQuad"
+              offset={-65}
+              duration={750}
+              onMouseEnter={(e) =>
+                e.currentTarget.classList.add(
+                  isDarkMode
+                    ? "temporary-hover-effect-dark"
+                    : "temporary-hover-effect-light"
+                )
+              }
+              onAnimationEnd={(e) => {
+                e.currentTarget.classList.remove("temporary-hover-effect-light")
+                e.currentTarget.classList.remove("temporary-hover-effect-dark")
+              }}
+              className="flex cursor-pointer items-center text-lg font-semibold text-slate-600 dark:text-slate-100 sm:text-sm"
+            >
+              {item.label}
+            </Link>
+          ))}
           <a
             href={siteConfig.links.resume}
             target="_blank"
             rel="noopener noreferrer"
-            ref={resumeLinkRef}
-            className="flex cursor-pointer items-center text-lg font-semibold text-slate-600 hover:text-slate-900 dark:text-slate-100 dark:hover:text-slate-500 sm:text-sm"
+            onMouseEnter={(e) =>
+              e.currentTarget.classList.add(
+                isDarkMode
+                  ? "temporary-hover-effect-dark"
+                  : "temporary-hover-effect-light"
+              )
+            }
+            onAnimationEnd={(e) => {
+              e.currentTarget.classList.remove("temporary-hover-effect-light")
+              e.currentTarget.classList.remove("temporary-hover-effect-dark")
+            }}
+            className="flex cursor-pointer items-center text-lg font-semibold text-slate-600 dark:text-slate-100 sm:text-sm"
           >
             Resume
           </a>
@@ -139,54 +113,20 @@ export function MainNav({ items }: MainNavProps) {
           sideOffset={-5}
           className="z-40 ml-[-8px] mt-[-47px] flex h-[100vh] w-[75vw] flex-col justify-center pl-6 md:hidden"
         >
-          <DropdownMenuItem>
-            <Link
-              to="home"
-              spy={true}
-              smooth={true}
-              offset={-65}
-              duration={750}
-              className="text-4xl"
-            >
-              <p onClick={onClick}>Home</p>
-            </Link>
-          </DropdownMenuItem>
-          <DropdownMenuItem>
-            <Link
-              to="portfolio"
-              spy={true}
-              smooth={true}
-              offset={-65}
-              duration={750}
-              className="text-4xl"
-            >
-              <p onClick={onClick}>Portfolio</p>
-            </Link>
-          </DropdownMenuItem>
-          <DropdownMenuItem>
-            <Link
-              to="aboutMe"
-              spy={true}
-              smooth={true}
-              offset={-65}
-              duration={750}
-              className="text-4xl"
-            >
-              <p onClick={onClick}>About Me</p>
-            </Link>
-          </DropdownMenuItem>
-          <DropdownMenuItem>
-            <Link
-              to="contact"
-              spy={true}
-              smooth={true}
-              offset={0}
-              duration={750}
-              className="text-4xl"
-            >
-              <p onClick={onClick}>Contact</p>
-            </Link>
-          </DropdownMenuItem>
+          {navItems.map((item) => (
+            <DropdownMenuItem key={item.to}>
+              <Link
+                to={item.to}
+                spy={true}
+                smooth={true}
+                offset={-65}
+                duration={750}
+                className="text-4xl"
+              >
+                <p onClick={onClick}>{item.label}</p>
+              </Link>
+            </DropdownMenuItem>
+          ))}
           <DropdownMenuItem>
             <a
               href={siteConfig.links.resume}
