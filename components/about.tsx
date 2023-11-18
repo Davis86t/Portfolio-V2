9377837fb10acfb187418dd2bd2ed24ce0b075f5
davis86t <4833583@gmail.com>
2023-11-17 18:50:41 -0600

    Fine tune spacing in About section

--- components/about.tsx
+++ components/about.tsx
@@ -9,13 +9,13 @@ export default function AboutMe() {
   return (
     <div
       id="aboutMe"
-      className="z-0 flex h-[calc(100dvh-65px)] max-h-[900px] min-h-[750px] flex-col justify-center border-b border-b-slate-200 py-16 dark:border-b-slate-700 sm:min-h-[680px]"
+      className="z-0 flex h-[calc(100dvh-65px)] max-h-[900px] min-h-[600px] flex-col justify-center border-b border-b-slate-200 py-16 dark:border-b-slate-700 sm:min-h-[680px]"
     >
       <section className="container flex h-full flex-col justify-center">
         <div className="flex flex-col items-start gap-4 sm:gap-0">
           <AboutMeAnimation />
-          <div className="flex min-h-[400px] flex-col overflow-auto rounded-lg bg-white p-4 dark:bg-slate-900 sm:m-0 sm:justify-center sm:p-0">
-            <ul className="max-w-[700px] list-disc space-y-2 pb-6 pl-5 text-slate-700 dark:text-slate-400 md:text-xl">
+          <div className="flex flex-col overflow-auto rounded-lg bg-white p-0 py-4 dark:bg-slate-900 sm:justify-center sm:pt-8">
+            <ul className="max-w-[700px] list-disc space-y-4 pb-6 pl-5 text-slate-700 dark:text-slate-400 md:text-xl">
               <li>
                 Transitioned from electronics and real estate to crafting web
                 apps.
@@ -33,7 +33,7 @@ export default function AboutMe() {
               </li>
             </ul>
 
-            <p className="text-md max-w-[700px] text-slate-700 dark:text-slate-400 md:text-xl">
+            <p className="text-md max-w-[700px] pt-4 text-slate-700 dark:text-slate-400 md:text-xl">
               Ready to bring my diverse expertise to your tech challenges?
               Let&apos;s connect:
             </p>
